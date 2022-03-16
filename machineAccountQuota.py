860622d7d81ada263be6df93022c09614ec9293f
TahiTi <flo.boud@gmail.com>
2022-03-16 17:21:45 +0100

    Description

--- machineAccountQuota.py
+++ machineAccountQuota.py
@@ -6,7 +6,6 @@
 # Description:
 #   This module will try to get the Machine Account Quota from the domain attribute ms-DS-MachineAccountQuota.
 #   If the value is superior to 0, it opens new paths to enumerate further the target domain.
-#   It is based on the impacket libraries.
 #
 #   Author:
 #       TahiTi
